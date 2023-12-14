34c8c210774b2488364e43ee638774ba75b444b8
gjgoh <gjgoh@autocrypt.io>
2023-12-14 18:23:28 +0900

    Add feature over read string

--- library/bignum.c
+++ library/bignum.c
@@ -465,6 +465,69 @@ cleanup:
     return ret;
 }
 
+int mbedtls_mpi_overRead_string(mbedtls_mpi *X, int radix, const char *s)
+{
+    int ret = MBEDTLS_ERR_ERROR_CORRUPTION_DETECTED;
+    size_t i, j, slen, n;
+    int sign = 1;
+    mbedtls_mpi_uint d;
+    mbedtls_mpi T;
+    MPI_VALIDATE_RET(X != NULL);
+    MPI_VALIDATE_RET(s != NULL);
+
+    if (radix < 2 || radix > 16) {
+        return MBEDTLS_ERR_MPI_BAD_INPUT_DATA;
+    }
+
+    mbedtls_mpi_init(&T);
+
+    if (s[0] == 0) {
+        mbedtls_mpi_free(X);
+        return 0;
+    }
+
+    if (s[0] == '-') {
+        ++s;
+        sign = -1;
+    }
+
+    slen = strlen(s);
+
+    if (radix == 16) {
+        if (slen > MPI_SIZE_T_MAX >> 2) {
+            return MBEDTLS_ERR_MPI_BAD_INPUT_DATA;
+        }
+
+        n = BITS_TO_LIMBS(slen << 2);
+
+        MBEDTLS_MPI_CHK(mbedtls_mpi_grow(X, n));
+        MBEDTLS_MPI_CHK(mbedtls_mpi_lset(X, 0));
+
+        for (i = slen, j = 0; i > 0; i--, j++) {
+            MBEDTLS_MPI_CHK(mpi_get_digit(&d, radix, s[i - 1]));
+            X->p[j / (2 * ciL)] |= d << ((j % (2 * ciL)) << 2);
+        }
+    } else {
+        MBEDTLS_MPI_CHK(mbedtls_mpi_lset(X, 0));
+
+        for (i = 0; i < slen; i++) {
+            MBEDTLS_MPI_CHK(mpi_get_digit(&d, radix, s[i]));
+            MBEDTLS_MPI_CHK(mbedtls_mpi_mul_int(&T, X, radix));
+            MBEDTLS_MPI_CHK(mbedtls_mpi_add_int(X, &T, d));
+        }
+    }
+
+    if (sign < 0 && mbedtls_mpi_bitlen(X) != 0) {
+        X->s = -1;
+    }
+
+cleanup:
+
+    mbedtls_mpi_free(&T);
+
+    return ret;
+}
+
 /*
  * Helper to write the digits high-order first.
  */
