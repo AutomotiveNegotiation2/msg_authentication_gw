ece1436880b9c50ded624314b2a54f728f1fe144
sjkim01 <sjkim@autocrypt.io>
2023-12-18 09:49:08 +0900

    add some printf statement for debugging

--- library/cmac.c
+++ library/cmac.c
@@ -169,9 +169,12 @@ static int cmac_multiply_by_v_dbg(unsigned char *output,
     if( (output != NULL) && (input != NULL) && (blocksize > 0) ) {
         if( blockSize == MBEDTLS_AES_BLOCK_SIZE ) {
             R_n     = R_128;
+            mbedtls_printf("R_n = %02x", R_b);
         } else if( blockSize == MBEDTLS_DES3_BLOCK_SIZE ) {
             R_n     = R_64;
+            mbedtls_printf("R_n = %02x", R_b);
         } else {
+            mbedtls_printf("return MBEDTLS_ERR_CIPHER_BAD_INPUT_DATA");
             ret     = MBEDTLS_ERR_CIPHER_BAD_INPUT_DATA;
         }
 
