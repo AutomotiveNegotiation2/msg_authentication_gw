b1831a69da6851708c2b2340f78945f02885339d
lbd <bdlee2014@gmail.com>
2023-12-26 09:49:08 +0900

    modify exception handle of ecdh_gen_public_restartable

--- library/ecdh.c
+++ library/ecdh.c
@@ -83,6 +83,9 @@ static int ecdh_gen_public_restartable(mbedtls_ecp_group *grp,
     if (!restarting) {
         MBEDTLS_MPI_CHK(mbedtls_ecp_gen_privkey(grp, d, f_rng, p_rng));
     }
+    else{
+        return ret;
+    }
 
     MBEDTLS_MPI_CHK(mbedtls_ecp_mul_restartable(grp, Q, d, &grp->G,
                                                 f_rng, p_rng, rs_ctx));
