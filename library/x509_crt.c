29faa897126daa04aa0157130bb244fbac69327a
lbd <bdlee2014@gmail.com>
2024-01-22 09:22:22 +0900

    modified return value x509_check_wildcard

--- library/x509_crt.c
+++ library/x509_crt.c
@@ -304,7 +304,7 @@ static int x509_check_wildcard(const char *cn, const mbedtls_x509_buf *name)
     }
 
     if (cn_idx == 0) {
-        return -1;
+        return -2;
     }
 
     if (cn_len - cn_idx == name->len - 1 &&
