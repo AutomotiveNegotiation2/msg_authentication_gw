d04c346804f7b821f5a4cf41de1dd04976274888
sjkim01 <sjkim@autocrypt.io>
2024-02-02 08:51:21 +0900

    do applying alignment for some local variable definitions.

--- library/ssl_tls13_generic.c
+++ library/ssl_tls13_generic.c
@@ -884,12 +884,12 @@ cleanup:
 
 int mbedtls_ssl_tls13_process_certificate_test(mbedtls_ssl_context *ssl)
 {
-    int ret = MBEDTLS_ERR_ERROR_CORRUPTION_DETECTED;
+    int             ret     = MBEDTLS_ERR_ERROR_CORRUPTION_DETECTED;
     MBEDTLS_SSL_DEBUG_MSG(2, ("=> parse certificate"));
 
 #if defined(MBEDTLS_SSL_TLS1_3_KEY_EXCHANGE_MODE_EPHEMERAL_ENABLED)
-    unsigned char *buf;
-    size_t buf_len;
+    unsigned char   *buf;
+    size_t          buf_len;
 
     MBEDTLS_SSL_PROC_CHK(mbedtls_ssl_tls13_fetch_handshake_msg(
                              ssl, MBEDTLS_SSL_HS_CERTIFICATE,
