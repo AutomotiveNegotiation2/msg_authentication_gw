c6741105c23ff53d65746f03141944aaec01dea1
lbd <bdlee2014@gmail.com>
2024-02-05 09:10:50 +0900

    exception handle x509_get_ext_key_usage

--- library/x509_crt.c
+++ library/x509_crt.c
@@ -597,6 +597,9 @@ static int x509_get_ext_key_usage(unsigned char **p,
     if ((ret = mbedtls_asn1_get_sequence_of(p, end, ext_key_usage, MBEDTLS_ASN1_OID)) != 0) {
         return MBEDTLS_ERROR_ADD(MBEDTLS_ERR_X509_INVALID_EXTENSIONS, ret);
     }
+    else {
+        /* do nothing */
+    }
 
     /* Sequence length must be >= 1 */
     if (ext_key_usage->buf.p == NULL) {
