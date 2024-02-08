5be1c440670a71fcf3e266f5d992a356a9e0e621
bsko <bsko@autocrypt.io>
2024-02-08 14:51:47 +0900

    add COOKIE_RETURN_SKIP

--- library/ssl_cookie.c
+++ library/ssl_cookie.c
@@ -41,6 +41,7 @@
 #define COOKIE_RETURN_ERROR         (-1)
 #define COOKIE_RETURN_WAITING       (1)
 #define COOKIE_RETURN_TIMEOUT       (2)
+#define COOKIE_RETURN_SKIP          (3)
 
 #if defined(MBEDTLS_USE_PSA_CRYPTO)
 #define PSA_TO_MBEDTLS_ERR(status) PSA_TO_MBEDTLS_ERR_LIST(status,   \
